921d713333d8f00653b90d655d9fadbf8d005e5f
aqqje <1042136232@qq.com>
2018-02-20 19:01:07 +0800

    update 2018-2-20 19:01

---
+++ src/day01/LOVE1.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class LOVE1 {
+
+	public static void main(String[] args) {
+		System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");//14
+		System.out.print("*"+"\t\t"+"*"+"\t\t\t\t"+"I love LIULI"+"\t\t\t\t\t"+"*"+"\t\t"+"*"+"\n");//13
+		System.out.print("\t"+"*"+"\t\t\t\t\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");//14
+		System.out.print("\t\t"+"*"+"\t\t\t\t\t\t\t\t\t\t"+"*"+"\t\t"+"\n");//14
+		System.out.print("\t\t\t"+"*"+"\t\t\t\t\t\t\t\t"+"*"+"\t"+"\n");//12
+		System.out.print("\t\t\t\t"+"*"+"\t\t\t\t\t\t"+"*"+""+"\t"+"\n");//11
+		System.out.print("\t\t\t\t\t"+"*"+"\t\t\t\t"+"*"+""+"\t\t"+"\n");//11
+		System.out.print("\t\t\t\t\t\t"+"*"+"\t\t"+"*"+""+"\t\t"+"\n");//11
+		System.out.print("\t\t\t\t\t\t\t"+"*"+"\n");//7
+	}
+}
---
+++ src/day01/LOVE2.java
@@ -0,0 +1,85 @@
+package day01;
+
+public class LOVE2 {
+	public static void main(String[] args) {
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("*");
+		System.out.print("\t");
+		//System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("I love java");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		
+		System.out.println("*");
+		
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("*");
+		System.out.print("\t");
+		
+		System.out.println("*");
+
+		
+		System.out.print("\t");
+		System.out.print("\t");
+		System.out.print("\t");
+		//System.out.print("\t");
+		System.out.print("      ");
+		System.out.print("*");
+	}
+}
